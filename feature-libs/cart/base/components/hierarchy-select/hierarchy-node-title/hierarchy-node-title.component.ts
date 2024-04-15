305f946c01887d153391fba99c8cbf6cd7ae21c7
qindegang <qindegang@gmail.com>
2024-04-15 17:22:11 +0800

    CXSPA-5727: Correct removeBundle

--- feature-libs/cart/base/components/hierarchy-select/hierarchy-node-title/hierarchy-node-title.component.ts
+++ feature-libs/cart/base/components/hierarchy-select/hierarchy-node-title/hierarchy-node-title.component.ts
@@ -6,7 +6,6 @@ import { Component, inject } from '@angular/core';
 import { HierarchyNodeComponent } from '../hierarchy-node/hierarchy-node.component';
 import { TitleNode } from '../title-node.model';
 import { ActiveCartFacade } from 'feature-libs/cart/base/root/facade/active-cart.facade';
-import { DOCUMENT } from '@angular/common';
 
 /**
  * Hierarchy Selection node variant to show a title
@@ -36,10 +35,8 @@ import { DOCUMENT } from '@angular/common';
 export class HierarchyNodeTitleComponent<T> extends HierarchyNodeComponent<T> {
   tree: TitleNode;
   activeCartService: ActiveCartFacade = inject(ActiveCartFacade);
-  document: Document = inject(DOCUMENT);
 
   removeBundle(entryGroupNumber: number) {
     this.activeCartService.removeEntryGroup(entryGroupNumber);
-    this.document.getElementById('bundle-' + entryGroupNumber)?.remove();
   }
 }
