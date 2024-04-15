8250cad31e0ec73b6ece3801b98bc684d352f566
qindegang <qindegang@gmail.com>
2024-04-15 14:36:03 +0800

    CXSPA-6771: Fix UT error

--- feature-libs/cart/base/components/hierarchy-select/hierarchy-node/hierarchy-node.component.spec.ts
+++ feature-libs/cart/base/components/hierarchy-select/hierarchy-node/hierarchy-node.component.spec.ts
@@ -11,81 +11,69 @@ import { TitleNode } from '../title-node.model';
 import { HierarchyNodeComponent } from './hierarchy-node.component';
 
 describe('HierarchyNodeComponent', () => {
-	let component: HierarchyNodeComponent<void>;
-	let fixture: ComponentFixture<HierarchyNodeComponent<void>>;
+  let component: HierarchyNodeComponent<void>;
+  let fixture: ComponentFixture<HierarchyNodeComponent<void>>;
 
-	beforeEach(
-		waitForAsync(() => {
-			TestBed.overrideComponent(HierarchyNodeComponent, {
-				set: {
-					providers: [],
-				},
-			})
-				.configureTestingModule({
-					declarations: [HierarchyNodeComponent],
-					providers: [],
-					schemas: [NO_ERRORS_SCHEMA],
-				})
-				.compileComponents();
-		})
-	);
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.overrideComponent(HierarchyNodeComponent, {
+        set: {
+          providers: [],
+        },
+      })
+        .configureTestingModule({
+          declarations: [HierarchyNodeComponent],
+          providers: [],
+          schemas: [NO_ERRORS_SCHEMA],
+        })
+        .compileComponents();
+    })
+  );
 
-	beforeEach(() => {
-		fixture = TestBed.createComponent<HierarchyNodeComponent<void>>(HierarchyNodeComponent);
-		component = fixture.componentInstance;
-	});
+  beforeEach(() => {
+    fixture = TestBed.createComponent<HierarchyNodeComponent<void>>(
+      HierarchyNodeComponent
+    );
+    component = fixture.componentInstance;
+  });
 
-	it('should create', () => {
-		expect(component).toBeTruthy();
-	});
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 
-	it('should add childPadding and paddingPrefix to generate childPaddingLeft', () => {
-		component.paddingPrefix = 100;
-		component.childPadding = 25;
+  it('should add childPadding and paddingPrefix to generate childPaddingLeft', () => {
+    component.paddingPrefix = 100;
+    component.childPadding = 25;
 
-		expect(component.childPaddingLeft).toBe(125);
-	});
+    expect(component.childPaddingLeft).toBe(125);
+  });
 
-	it("should expose tree node's disabled status", () => {
-		const mockNode = new HierarchyNode('root');
-		component.tree = mockNode;
+  it("should expose tree node's disabled status", () => {
+    const mockNode = new HierarchyNode('root');
+    component.tree = mockNode;
 
-		expect(component.disabled).toBe(mockNode.disabled);
+    expect(component.disabled).toBe(mockNode.disabled);
 
-		mockNode.disabled = true;
+    mockNode.disabled = true;
 
-		expect(component.disabled).toBe(mockNode.disabled);
-	});
+    expect(component.disabled).toBe(mockNode.disabled);
+  });
 
-	describe('seting the type on init', () => {
-		it('should set NODE for null', () => {
-			component.ngOnInit();
+  describe('seting the type on init', () => {
+    it('should set NODE for CollapsibleNode', () => {
+      component.tree = new CollapsibleNode();
 
-			expect(component.type).toEqual('NODE');
-		});
+      component.ngOnInit();
 
-		it('should set NODE for HierarchyNode', () => {
-			component.tree = new HierarchyNode();
+      expect(component.type).toEqual('COLLAPSIBLE');
+    });
 
-			component.ngOnInit();
+    it('should set NODE for TitleNode', () => {
+      component.tree = new TitleNode();
 
-			expect(component.type).toEqual('NODE');
-		});
+      component.ngOnInit();
 
-		it('should set NODE for CollapsibleNode', () => {
-			component.tree = new CollapsibleNode();
-
-			component.ngOnInit();
-
-			expect(component.type).toEqual('COLLAPSIBLE');
-		});
-
-		it('should set NODE for TitleNode', () => {
-			component.tree = new TitleNode();
-
-			component.ngOnInit();
-
-			expect(component.type).toEqual('TITLE');
-		});
-	});
+      expect(component.type).toEqual('TITLE');
+    });
+  });
 });
